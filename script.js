9c4fd8d50e38b26a18894718a77acd07ae390000
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-12-01 18:04:03 +0100

    Checking that the password fields match each other

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ document
   .addEventListener("click", function () {
     togglePasswordVisibility("confirmPassword", "toggleConfirmPassword");
   });
+
+function checkPasswordMatch() {
+  let password = document.getElementById("password");
+  let confirmPassword = document.getElementById("confirmPassword");
+  let errorDiv = document.getElementById("passwordError");
+
+  if (password.value !== confirmPassword.value) {
+    errorDiv.textContent = "*Passwords do not match.";
+  } else {
+    errorDiv.textContent = "";
+  }
+}
+
+document
+  .getElementById("password")
+  .addEventListener("input", checkPasswordMatch);
+document
+  .getElementById("confirmPassword")
+  .addEventListener("input", checkPasswordMatch);
